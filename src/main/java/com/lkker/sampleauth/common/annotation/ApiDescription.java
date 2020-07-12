970851075ac451e83bd2d15bdec5ee6404ba2bed
liliang <dzliliang@126.com>
2020-07-12 21:43:26 +0800

    commit

--- src/main/java/com/lkker/sampleauth/common/annotation/ApiDescription.java
+++ src/main/java/com/lkker/sampleauth/common/annotation/ApiDescription.java
@@ -1,5 +1,12 @@
 package com.lkker.sampleauth.common.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ApiDescription {
     /**
      * 描述
