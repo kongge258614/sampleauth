bdfe47e68d3362fe7cc53cc06e93d1ef36c11212
liliang <dzliliang@126.com>
2020-07-13 22:16:10 +0800

    commit

--- src/main/java/com/lkker/sampleauth/constant/ConstantKey.java
+++ src/main/java/com/lkker/sampleauth/constant/ConstantKey.java
@@ -3,4 +3,18 @@ package com.lkker.sampleauth.constant;
 public class ConstantKey {
 
     public static final String SIGNING_KEY = "spring-security-@Jwt!&Secret^#";
+
+    public static class userRole{
+
+        /**
+         * 超级管理员
+         */
+        public static final String ADMIN = "ADMIN";
+
+        /**
+         * 普通成员
+         */
+        public static final String MEMBER = "MEMBER";
+
+    }
 }
